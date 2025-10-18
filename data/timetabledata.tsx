2d2c86cd00bff7862d8605a3156db7d88f9a3b74
aftabalikhandeveloper <aftabalikhan900@gmail.com>
2025-10-18 22:11:06 +0500

    Changing names of teacher

--- data/timetabledata.tsx
+++ data/timetabledata.tsx
@@ -7,16 +7,16 @@
 
 export const timetable = {
   'Monday': [
-    { time: '08:30 AM', from_to: '08:30 AM to 10:00 AM', subject: 'mg100', teacher: 'Muhammad Shahid Jan', room: 'EB/C-2' },
-    { time: '10:00 AM', from_to: '10:00 AM to 11:30 AM', subject: 'cs210', teacher: 'TUFAIL MUHAMMAD', room: 'EB/A4' },
-    { time: '11:30 AM', from_to: '11:30 AM to 01:00 PM', subject: 'mt221', teacher: 'HAMID QADRI', room: 'EB/C-7' },
-    { time: '01:30 PM', from_to: '01:30 PM to 03:00 PM', subject: 'SE253', teacher: 'Nisar Ali', room: 'EB/D11' },
+    { time: '08:30 AM', from_to: '08:30 AM to 10:00 AM', subject: 'mg100', teacher: 'Sir Muhammad Shahid Jan', room: 'EB/C-2' },
+    { time: '10:00 AM', from_to: '10:00 AM to 11:30 AM', subject: 'cs210', teacher: 'Sir TUFAIL MUHAMMAD', room: 'EB/A4' },
+    { time: '11:30 AM', from_to: '11:30 AM to 01:00 PM', subject: 'mt221', teacher: 'Sir HAMID QADRI', room: 'EB/C-7' },
+    { time: '01:30 PM', from_to: '01:30 PM to 03:00 PM', subject: 'SE253', teacher: 'Sir Nisar Ali', room: 'EB/D11' },
     null
   ],
 
   'Tuesday': [
     null,
-    { time: '10:00 AM', from_to: '10:00 AM to 11:30 AM', subject: 'cs210', teacher: 'TUFAIL MUHAMMAD', room: 'EB/C13' },
+    { time: '10:00 AM', from_to: '10:00 AM to 11:30 AM', subject: 'cs210', teacher: 'Sir TUFAIL MUHAMMAD', room: 'EB/C13' },
     null,
     null,
     null
@@ -25,24 +25,24 @@ export const timetable = {
   'Wednesday': [
     null,
     null,
-    { time: '11:30 AM', from_to: '11:30 AM to 01:00 PM', subject: 'cs210 (Lab)', teacher: 'Mam Faiza', room: 'Cyber Security' },
-    { time: '01:30 PM', from_to: '01:30 PM to 03:00 PM', subject: 'cs210 (Lab)', teacher: 'Mam Faiza', room: 'Cyber Security' },
+    { time: '11:30 AM', from_to: '11:30 AM to 01:00 PM', subject: 'cs210 (Lab)', teacher: 'Mam Faryal', room: 'Cyber Security' },
+    { time: '01:30 PM', from_to: '01:30 PM to 03:00 PM', subject: 'cs210 (Lab)', teacher: 'Mam Faryal', room: 'Cyber Security' },
     null
   ],
 
   'Thursday': [
-    { time: '08:30 AM', from_to: '08:30 AM to 10:00 AM', subject: 'mg100', teacher: 'Muhammad Shahid Jan', room: 'EB/C-2' },
-    { time: '10:00 AM', from_to: '10:00 AM to 11:30 AM', subject: 'mt221', teacher: 'HAMID QADRI', room: 'EB/C-7' },
+    { time: '08:30 AM', from_to: '08:30 AM to 10:00 AM', subject: 'mg100', teacher: 'Sir Muhammad Shahid Jan', room: 'EB/C-2' },
+    { time: '10:00 AM', from_to: '10:00 AM to 11:30 AM', subject: 'mt221', teacher: 'Sir HAMID QADRI', room: 'EB/C-7' },
     null,
     null,
     null
   ],
 
   'Friday': [
-    { time: '08:30 AM', from_to: '08:30 AM to 10:00 AM', subject: 'SE253', teacher: 'Nisar Ali', room: 'EB/C-7' },
+    { time: '08:30 AM', from_to: '08:30 AM to 10:00 AM', subject: 'SE253', teacher: 'Sir Nisar Ali', room: 'EB/C-7' },
     null,
-    { time: '11:30 AM', from_to: '11:30 AM to 01:00 PM', subject: 'cs252', teacher: 'Sunbal Rani', room: 'EB/D15' },
-    { time: '01:30 PM', from_to: '01:30 PM to 03:00 PM', subject: 'cs252 (Lab)', teacher: 'Numan', room: 'AI Lab' },
-    { time: '03:00 PM', from_to: '03:00 PM to 04:30 PM', subject: 'cs252 (Lab)', teacher: 'Numan', room: 'AI Lab' }
+    { time: '11:30 AM', from_to: '11:30 AM to 01:00 PM', subject: 'cs252', teacher: 'Mam Sunbal Rani', room: 'EB/D15' },
+    { time: '01:30 PM', from_to: '01:30 PM to 03:00 PM', subject: 'cs252 (Lab)', teacher: 'Sir Numan', room: 'AI Lab' },
+    { time: '03:00 PM', from_to: '03:00 PM to 04:30 PM', subject: 'cs252 (Lab)', teacher: 'Sir Numan', room: 'AI Lab' }
   ]
 };
